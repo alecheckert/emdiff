cbfa7ff260ff52d5552384edb9a27fc3b8b142a4
Alec Heckert <alecheckert@gmail.com>
2024-02-03 17:03:47 -0500

    add test for empty input

--- tests/test_vbdiff.py
+++ tests/test_vbdiff.py
@@ -29,3 +29,23 @@ class TestVBDiff(TestCase):
         assert (occs >= 0.0).all()
         assert (diff_coefs >= 0.0).all()
         assert abs(occs.sum() - 1.0) < 1e-5
+
+    def test_empty(self):
+        spots = pd.DataFrame(
+            {
+                "x": np.zeros(0, dtype=np.float64),
+                "y": np.zeros(0, dtype=np.float64),
+                "trajectory": np.zeros(0, dtype=np.int64),
+                "frame": np.zeros(0, dtype=np.int64),
+            }
+        )
+        n_states = 2
+        with self.assertRaises(RuntimeError):
+            occs, diff_coefs = vbdiff(
+                spots,
+                n_states=n_states,
+                pixel_size_um=0.16,
+                frame_interval=0.00748,
+                loc_error=0.035,
+                dz=0.7,
+            )
